6379dc0fa579ccb7f49470586e730a9730e23ade
bungongtoto <kingsley.bungong@ictuniversity.edu.cm>
2023-09-02 14:08:44 +0100

    consuming graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+export type Launch = {
+
+    "mission_name": string,
+    "launch_date_local": string,
+    "launch_site": {
+      "site_name_long": string,
+    }
+ 
+}
+
+const GET_DATA = gql`
+{
+  launchesPast(limit: 10) {
+    mission_name
+    launch_date_local
+    launch_site {
+      site_name_long
+    }
+  }
+}
+`;
+
 
 function App() {
+  const {loading, error, data} = useQuery(GET_DATA);
+
+  useEffect(() => {
+    console.log(loading, error, data);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data ? 
+        data.launchesPast.map((launch: Launch) => {
+          return (
+            <p>{launch.mission_name +' '+ launch.launch_date_local}</p>
+          );
+        })  
+      : null}
     </div>
   );
 }
